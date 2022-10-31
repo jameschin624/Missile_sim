d66add6585b4f655983e4cc26f85a771ba45d1ac
jameschin624 <117089260+jameschin624@users.noreply.github.com>
2022-11-01 00:31:44 +0800

    Delete test directory

--- test/standard.m
+++
@@ -1,9 +0,0 @@
-[s] = textread('standard.txt');
-x=s(:,1);
-y=s(:,2);
-z=s(:,3);
-xx=0.1:0.05:0.95;
-plot(xx,x,'ro-');hold on;plot(xx,y,'g*-');plot(xx,z,'b+-');hold off;
-xlabel('coefficient for acceleration function');
-ylabel('standard deviation');
-
